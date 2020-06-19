a1bab87363d57fe5f9f927dd3bebc0e21fd4d629
shivani-vnode <shivani.thakur@vdotnode.com>
2020-06-19 23:48:56 +0530

    jwt config add

--- src/main/java/com/oauth2/example/authorizationserver/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/oauth2/example/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -1,15 +1,20 @@
 package com.oauth2.example.authorizationserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -24,6 +29,8 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
     private AuthenticationManager authenticationManager;
     @Autowired
 	private TokenStore tokenStore;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
 
 /*    @Bean
@@ -58,7 +65,15 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         //endpoints.tokenStore(jdbcTokenStore());
-        endpoints.tokenStore(tokenStore);
+       // endpoints.tokenStore(tokenStore);
+    	endpoints.accessTokenConverter(accessTokenConverter());
+        endpoints.userDetailsService(userDetailsService);
         endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        return converter;
     }
 }
